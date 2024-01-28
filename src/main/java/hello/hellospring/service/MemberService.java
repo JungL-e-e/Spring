578d105ce48b2bd9793c7cfa0dfab45031583f53
JungLee <ljung412@gmail.com>
2024-01-29 01:35:13 +0900

    Spring - Spring bean and Dependency Injection

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//  @Service         컴포넌트 스캔 사용법
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+//    @Autowired         컴포넌트 스캔 사용법
     public MemberService(MemberRepository memberRepository) {   // 이걸로 인해서 테스트가 편해짐, 추가공부 필요
         this.memberRepository = memberRepository;
     }
